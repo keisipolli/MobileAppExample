051e044909a5a7265782d4c4f771b9d7c87029c9
keisipolli <keisi.polli@khk.ee>
2023-02-10 09:44:52 +0200

    Create authorization screens

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-import Splash from "./src/screens/auth/Splash";
+import React, { useEffect } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import Signin from "./src/screens/auth/SignIn";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from "react-native-config";
+
+/*const WEB_CLIENT_ID = '' 
+const IOS_CLIENT_ID = ''*/
 
 const App = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+      webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+      offlineAccess: true,
+      iosClientId: Config.GOOGLE_IOS_CLIENT_ID,
+      /*webClientId: WEB_CLIENT_ID,
+      offlineAccess: true,
+      iosClientId: IOS_CLIENT_ID,*/
+    })
+  }, [])
   return (
+    <ScrollView>
   <SafeAreaView>
-    <Splash />
+    <Signin />
   </SafeAreaView>
+    </ScrollView>
+
   );
 };
 
-export default App
+export default React.memo(App)
